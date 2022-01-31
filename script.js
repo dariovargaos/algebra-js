50e9477f7fb41fe8e6d1280488c47abb287ec609
dariovargaos <71967741+dariovargaos@users.noreply.github.com>
2022-01-31 21:16:09 +0100

    API exercise
    add API exercise and JS bible

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-// prvi zadatak
-var firstName = "Dario";
-var lastName = "Varga";
-var fullName = firstName + " " +  lastName;
-
-console.log(fullName);
-
-//drugi zadatak
-
-
-//treci zadatak
-for(num = 1; num <= 20; num++){
-    if(num % 2 == 0){
-        console.log("Broj " + num + " je paran.");
-    }
-    else{
-        console.log("Broj " + num + " je neparan.");
-    }
+function Dog(name){
+    this.name = name || "Rex";
+    this.bark = function(){
+        return "wuf wuf";
+    };
 }
 
-//cetvrti zadatak
-var array = ["Ivana", "Sanjin", "Marijan", "Doroti", "Damir", "Lorena", "Ružica"];
-array.forEach(element => console.log(element));
+var dog1 = new Dog(Snoopy);
 
-var array2 = [
-    ["Dijete", "Odrasla osoba"],
-    ["Računalo", "Pisača mašina"],
-    ["Mokro", "Suho"]
+Dog.prototype.getName = function(){
+    return this.name;
+};
 
-];
-
-console.table(array2);
-
-array2.push(["Gladan", "Sit"]);
-
-console.table(array2);
+Snoopy.getName();
